9dfbe181efed2b8ffa711fcee2bf0bbd3de06c39
Emilio Mendoza <ironislife39@gmail.com>
2024-10-15 13:38:22 -0400

    Simple HTTP Server

--- main.go
+++ main.go
@@ -17,3 +17,18 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
 }
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server closed\n")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+
+	}
+}
